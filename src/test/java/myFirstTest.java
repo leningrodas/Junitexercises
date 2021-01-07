26b9f6abf29391af6a192f9136fd4761a15723e7
lening rodas <lening.a.rodas@gmail.com>
2021-01-07 10:30:12 -0600

    warm up exercises

---
+++ src/test/java/myFirstTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class myFirstTest {
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonnedSheep = sheep;
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonnedSheep);
+    }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+
+
+
+
+
+}
